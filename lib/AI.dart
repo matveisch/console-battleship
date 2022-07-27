c6ca0d45488764df6da57af9daa4035044e0f21c
Matvei <matvesch@protonmail.com>
2022-07-27 19:31:08 +0300

    no more same coordinates in ai

--- lib/AI.dart
+++ lib/AI.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
 import 'dart:math';
 
 class AI {
   bool lastHitSuccessful = false;
-  List usedCoordinates = [];
-  List targets = [];
+  List<Map> previousRandomCoordinates = [];
+  List<Map> targets = [];
+  List<Map> usedCoordinates = [];
 
   // to generate random nums
   Random random = new Random();
@@ -16,16 +18,26 @@ class AI {
     // if last attack fails and there are no presaved targets
     if (lastHitSuccessful == false && targets.isEmpty) {
       coordinates = {'x': randomNum(1, 10), 'y': randomNum(1, 10)};
-      usedCoordinates.add(coordinates);
+      
+      // check if random coordinates are the new ones
+      if (usedCoordinates.isNotEmpty) {
+        usedCoordinates.forEach((element) {
+          if (element == coordinates) {
+            coordinates = {'x': randomNum(1, 10), 'y': randomNum(1, 10)};
+          }
+        });
+      }
+
+      previousRandomCoordinates.add(coordinates);
     }
     
     // if last hit successful and there are no presaved targets
     if (lastHitSuccessful == true && targets.isEmpty) {
       // add several coordinates based on last coordinates
-      targets.add({'x': usedCoordinates.last['x'] + 1, 'y': usedCoordinates.last['y']});
-      targets.add({'x': usedCoordinates.last['x'], 'y': usedCoordinates.last['y'] + 1});
-      targets.add({'x': usedCoordinates.last['x'] - 1, 'y': usedCoordinates.last['y']});
-      targets.add({'x': usedCoordinates.last['x'], 'y': usedCoordinates.last['y'] - 1});
+      targets.add({'x': previousRandomCoordinates.last['x'] + 1, 'y': previousRandomCoordinates.last['y']});
+      targets.add({'x': previousRandomCoordinates.last['x'], 'y': previousRandomCoordinates.last['y'] + 1});
+      targets.add({'x': previousRandomCoordinates.last['x'] - 1, 'y': previousRandomCoordinates.last['y']});
+      targets.add({'x': previousRandomCoordinates.last['x'], 'y': previousRandomCoordinates.last['y'] - 1});
     } 
     
     // if there are presaved targets
@@ -47,6 +59,7 @@ class AI {
     print(lastHitSuccessful);
     print(board[coordinates['y']][coordinates['x']]);
 
+    usedCoordinates.add(coordinates);
     return coordinates;
   }
 }
